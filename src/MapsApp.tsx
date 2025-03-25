17dbf3206c0bd8b4e17ccad92c1c2dbefd485a68
‘Freddy <elipson39z@gmail.com>
2025-03-25 16:17:06 -0500

    initial configuration

---
+++ src/MapsApp.tsx
@@ -0,0 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { StackNavigator } from './presentation/navigation/StackNavigator'
+
+export const MapsApp = () => {
+    return (
+        <NavigationContainer>
+            <StackNavigator/>
+        </NavigationContainer>
+    )
+}
